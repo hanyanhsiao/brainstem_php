c13db6b4ea4bc5238f703777b73cefa92097b679
chene3 <2017gsb.chenes@gmail.com>
2023-06-03 14:07:54 +0800

    mail Win測試

--- forget.php
+++ forget.php
@@ -9,6 +9,7 @@ require 'vendor/autoload.php';
 $email = $_POST['email'];
 $verificationCode = $_POST['verificationCode'];
 
+
 // 构建查询语句，检查是否存在匹配的MEMBER_ACCOUNT
 $sql = "SELECT COUNT(*) FROM MEMBER_DATA WHERE MEMBER_ACCOUNT = ?";
 $statement = $pdo->prepare($sql);
@@ -31,15 +32,15 @@ if ($result > 0) {
     $mail->SMTPAutoTLS = false;
     $mail->CharSet = 'UTF-8';
     $mail->Encoding = 'base64';
-    $mail->Username = 'chenyse3@gmail.com'; // 你的 Gmail 邮箱地址
-    $mail->Password = ''; // 你的 Gmail 邮箱密码
+    $mail->Username = 'brainstem.thd101@gmail.com'; // 你的 Gmail 邮箱地址
+    $mail->Password = 'gqwjsvzsikiinomr'; // 你的 Gmail 邮箱密码
 
     // 设置发件人姓名
-    $mail->setFrom('chenyse3@gmail.com', 'Brainstem'); // 替换为你的发件人姓名和邮箱地址
+    $mail->setFrom('brainstem.thd101@gmail.com', 'Brainstem'); // 替换为你的发件人姓名和邮箱地址
 
     // 设置收件人邮箱地址和姓名
     $mail->addAddress($email); // 使用用户填写的邮箱地址作为收件人
-    $mail->addReplyTo('chenyse3@gmail.com', 'Brainstem'); // 替换为你的回复邮箱地址和姓名
+    $mail->addReplyTo('brainstem.thd101@gmail.com', 'Brainstem'); // 替换为你的回复邮箱地址和姓名
 
     // 设置邮件主题和内容
     $mail->Subject = 'Brainstem忘記密碼_驗證碼';
@@ -47,21 +48,27 @@ if ($result > 0) {
 
     // 发送邮件
     if ($mail->send()) {
-       // 更新数据库密码
-       $updateSql = "UPDATE MEMBER_DATA SET MEMBER_PASSWORD = ? WHERE MEMBER_ACCOUNT = ?";
-       $updateStatement = $pdo->prepare($updateSql);
-       $updateStatement->bindValue(1, $newPassword);
-       $updateStatement->bindValue(2, $email);
-       $updateResult = $updateStatement->execute();
+        if (isset($_POST['verificationCode']) && isset($_POST['newPassword'])) {
+            // 更新数据库密码
+            $new_password = $_POST['newPassword'];
+            $updateSql = "UPDATE MEMBER_DATA SET MEMBER_PASSWORD = ? WHERE MEMBER_ACCOUNT = ?";
+            $updateStatement = $pdo->prepare($updateSql);
+            $updateStatement->bindValue(1, $new_password);
+            $updateStatement->bindValue(2, $email);
+            $updateResult = $updateStatement->execute();
 
-       if ($updateResult) {
-           echo '0';
-       } else {
-           echo '1';
-       }
+            if ($updateResult) {
+                echo '0';
+            } else {
+                echo '1';
+            }
+        } else {
+            echo '2'; // 只发送了邮件，没有提供验证码和新密码
+        }
     } else {
         echo '发送邮件失败：' . $mail->ErrorInfo;
     }
+ 
 } else {
     echo '1'; // 不存在匹配的MEMBER_ACCOUNT
 }
