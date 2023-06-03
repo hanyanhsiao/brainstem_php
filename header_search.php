ab6e1710864bd531a0c306ef318398c9657bfecd
Damian19881010 <a3cvbn@gmail.com>
2023-06-03 13:29:56 +0800

    [hreader_search.php] 打開資料庫大門、API服務生送單

--- header_search.php
+++ header_search.php
@@ -4,17 +4,21 @@
 
 //資料庫連線(這行一定要寫!!!!!!!!!)
 include("Conn.php");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 // 撰寫 SQL 查詢
 // $sql = "SELECT * FROM 你要撈的資料表";
-$sql = "SELECT GAME_COVER, GAME_NAME FROM GAME_DATA";
 
-
+$keyword = $_GET['keyword']; // 获取关键字参数的值
+$sql = "SELECT GAME_COVER, GAME_NAME FROM GAME_DATA WHERE GAME_NAME LIKE ?";
 
 
 // 執行查詢
 $statement = $pdo->prepare($sql);
-$statement->execute();
+// $statement->execute();
+$statement->execute(["%$keyword%"]); // 將關鍵字綁定到查詢中的LIKE子句
 
 //抓出全部且依照順序封裝成一個二維陣列
 // $testResult  = $statement->fetchAll();
@@ -36,7 +40,7 @@ if (count($results) > 0) {
 // -----------在這上面做資料處理回傳你想要的格式-----------------
 
 // 關閉資料庫連線
-unset($pdo); 
+// unset($pdo); 
 
 // 轉換成 JSON 格式輸出 
 echo json_encode($jsonArray);
